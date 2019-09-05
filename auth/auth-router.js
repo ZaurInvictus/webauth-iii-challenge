ad798fa3b6e3ec23b232c0e2aac12a0cc0c22478
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-09-05 15:01:54 -0500

    POST mothod to /api/login endpoint created

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,3 +24,46 @@ router.post('/register', (req, res) => {
 
 
 // LOGIN
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        
+        // TOKEN
+        const token = getJwt(user)
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+
+
+// GENERATING TOKEN
+function getJwt(user) {
+  const payload = {
+     // subject is normally the user's id (who/what the token describes)
+     subject: user.id, //translates into the "sub" property on the token
+     username: user.username,
+     jwtid: 1
+  }
+  const options = {
+    expiresIn: '8h'
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
+
+
+module.exports = router
+
