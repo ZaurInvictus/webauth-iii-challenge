0bf52ec186f0136fb66ffdafee064ce975da7f7a
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-09-10 22:10:40 -0500

    POST method to register updated

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -9,18 +9,21 @@ const secrets = require('../config/secrets.js')
 // REGISTER
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
-  let user = req.body
-  const hash = bcrypt.hashSync(user.password, 10) // 2 ^ n
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
 
   Users.add(user)
     .then(saved => {
+      // TOKEN
+      // const token = getJwt(user)
+      // res.status(201).json({ saved, token });
       res.status(201).json(saved);
     })
     .catch(error => {
       res.status(500).json(error);
-    })
-})
+    });
+});
 
 
 // LOGIN
